32826938ce427267e0cb0d6385698f8876bce9ee
Roi Kingon <rkingon@gmail.com>
2022-10-10 20:32:42 -0700

    move cms url to env var

--- src/components/Craft/index.tsx
+++ src/components/Craft/index.tsx
@@ -2,7 +2,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import { PropsWithChildren } from 'react'
 
 export const client = new ApolloClient({
-	uri: 'https://nextjs-poc.craftcms.me/api',
+	uri: process.env.CMS_URL,
 	cache: new InMemoryCache()
 })
 
